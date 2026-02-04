d6d8696eae38f574f1f1781c8f408cad78d4a7ea
leosajaaaa <pamankamranjeyus@gmail.com>
2026-02-04 17:33:15 +0700

    token.json

--- leo.js
+++ leo.js
@@ -1,6 +1,6 @@
-{
-   "status" : "on",
-  "allowed_numbers": [
-    "6285185911564"
-  ]
+{ 
+    "tokens": [
+       "8145068747:AAE2_sI4VzU4epAaAB_Y6meS5g3BgwscyYw",
+       "8447944956:AAE0an7M0YuJYHVoOrKOcDTGS8YWDEmUOdo"
+       ]
 }
